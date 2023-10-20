5e6db1a44e0f232429d548e8891a0e19780a5a5d
Hunter Empey <LizardMak@gmail.com>
2023-10-20 12:09:19 -0600

    removed 3rd options

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,5 @@ window.addEventListener("load", function() {
 function getResults(e) {
 e.preventDefault();
 window.alert("edefaultworking");
-let finalResultsPython = calculatePython();
-let finalResultsRuby = calculateRuby();
-let finalResultsC = calculateC();
-console.log("python is: " + finalResultsPython);
-console.log("ruby is: " + finalResultsRuby);
-console.log("C is: " + finalResultsC);
+let results = 0;
 }
