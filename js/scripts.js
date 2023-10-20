fc22f6cd1b6f70dff0ca5c15c149a8386d817e0d
Hunter Empey <LizardMak@gmail.com>
2023-10-20 13:14:35 -0600

    split functions into business & UI

--- js/scripts.js
+++ js/scripts.js
@@ -2,66 +2,55 @@ window.addEventListener("load", function() {
   document.getElementById("radio-form").addEventListener("submit", getResults);
 })
 
-function getResults(e) {
-e.preventDefault();
-window.alert("edefaultworking");
-let results = 0;
-let inputError = false;
-if (document.querySelector("input[name='name']:checked").value === "positive") {
-  results = results + 1;
-} else if (document.querySelector("input[name='name']:checked").value === "negative") {
-  results = results - 1;
-} else {
-  inputError = true;
-};
-if (document.querySelector("input[name='name2']:checked").value === "positive") {
-  results = results + 1;
-} else if (document.querySelector("input[name='name2']:checked").value === "negative") {
-  results = results - 1;
-} else {
-  inputError = true;
-};
-if (document.querySelector("input[name='name3']:checked").value === "positive") {
-  results = results + 1;
-} else if (document.querySelector("input[name='name3']:checked").value === "negative") {
-  results = results - 1;
-} else {
-  inputError = true;
-};
-if (document.querySelector("input[name='name4']:checked").value === "positive") {
-  results = results + 1;
-} else if (document.querySelector("input[name='name4']:checked").value === "negative") {
-  results = results - 1;
-} else {
-  inputError = true;
-};
-if (document.querySelector("input[name='name5']:checked").value === "positive") {
-  results = results + 1;
-} else if (document.querySelector("input[name='name5']:checked").value === "negative") {
-  results = results - 1;
-} else {
-  inputError = true;
-};
-if (document.querySelector("input[name='name6']:checked").value === "positive") {
-  results = results + 1;
-} else if (document.querySelector("input[name='name6']:checked").value === "negative") {
-  results = results - 1;
-} else {
-  inputError = true;
-};
-if (document.querySelector("input[name='name7']:checked").value === "positive") {
-  results = results + 1;
-} else if (document.querySelector("input[name='name7']:checked").value === "negative") {
-  results = results - 1;
-} else {
-  inputError = true;
-};
-if (document.querySelector("input[name='name8']:checked").value === "positive") {
-  results = results + 1;
-} else if (document.querySelector("input[name='name8']:checked").value === "negative") {
-  results = results - 1;
-} else {
-  inputError = true;
-};
-console.log(results) 
+function calculateResults() {
+  let results = 0;
+  if (document.querySelector("input[name='name']:checked").value === "positive") {
+    results = results + 1;
+  } else if (document.querySelector("input[name='name']:checked").value === "negative") {
+    results = results - 1;
+  };
+  if (document.querySelector("input[name='name2']:checked").value === "positive") {
+    results = results + 1;
+  } else if (document.querySelector("input[name='name2']:checked").value === "negative") {
+    results = results - 1;
+  };
+  if (document.querySelector("input[name='name3']:checked").value === "positive") {
+    results = results + 1;
+  } else if (document.querySelector("input[name='name3']:checked").value === "negative") {
+    results = results - 1;
+  };
+  if (document.querySelector("input[name='name4']:checked").value === "positive") {
+    results = results + 1;
+  } else if (document.querySelector("input[name='name4']:checked").value === "negative") {
+    results = results - 1;
+  };
+  if (document.querySelector("input[name='name5']:checked").value === "positive") {
+    results = results + 1;
+  } else if (document.querySelector("input[name='name5']:checked").value === "negative") {
+    results = results - 1;
+  };
+  if (document.querySelector("input[name='name6']:checked").value === "positive") {
+    results = results + 1;
+  } else if (document.querySelector("input[name='name6']:checked").value === "negative") {
+    results = results - 1;
+  };
+  if (document.querySelector("input[name='name7']:checked").value === "positive") {
+    results = results + 1;
+  } else if (document.querySelector("input[name='name7']:checked").value === "negative") {
+    results = results - 1;
+  };
+  if (document.querySelector("input[name='name8']:checked").value === "positive") {
+    results = results + 1;
+  } else if (document.querySelector("input[name='name8']:checked").value === "negative") {
+    results = results - 1;
+  };
+  console.log("should be: " + results)
+  return results; 
 }
+
+function getResults(e) {
+  e.preventDefault();
+  window.alert("edefaultworking");
+  let finalResults = calculateResults();
+  console.log("is: " + finalResults);
+  }
