db5dcc53f87037f5af6dcde9ff6f4e13abe1dbdf
Hunter Empey <LizardMak@gmail.com>
2023-10-20 11:53:19 -0600

    finding impossible equal results (WIP)

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,14 @@ function calculatePython() {
   const question1Value = document.querySelector("input[name='name']:checked").value;
   const question2Value = document.querySelector("input[name='name2']:checked").value;
   const question3Value = document.querySelector("input[name='name3']:checked").value;
+  const question4Value = document.querySelector("input[name='name4']:checked").value;
+  const question5Value = document.querySelector("input[name='name5']:checked").value;
+  const question6Value = document.querySelector("input[name='name6']:checked").value;
+  const question7Value = document.querySelector("input[name='name7']:checked").value;
+  const question8Value = document.querySelector("input[name='name8']:checked").value;
+  const question9Value = document.querySelector("input[name='name9']:checked").value;
+  const question10Value = document.querySelector("input[name='name10']:checked").value;
+  const question11Value = document.querySelector("input[name='name11']:checked").value;
   if (question1Value === "python") {
     resultsPython = resultsPython + 1;
   } else {
@@ -33,6 +41,46 @@ function calculatePython() {
   } else {
     resultsPython = resultsPython + 0;
   };
+  if (question4Value === "python") {
+    resultsPython = resultsPython + 1;
+  } else {
+    resultsPython = resultsPython + 0;
+  };
+   if (question5Value === "python") {
+    resultsPython = resultsPython + 1;
+  } else {
+    resultsPython = resultsPython + 0;
+  };
+   if (question6Value === "python") {
+    resultsPython = resultsPython + 1;
+  } else {
+    resultsPython = resultsPython + 0;
+  };
+  if (question7Value === "python") {
+    resultsPython = resultsPython + 1;
+  } else {
+    resultsPython = resultsPython + 0;
+  };
+  if (question8Value === "python") {
+    resultsPython = resultsPython + 1;
+  } else {
+    resultsPython = resultsPython + 0;
+  };
+  if (question9Value === "python") {
+    resultsPython = resultsPython + 1;
+  } else {
+    resultsPython = resultsPython + 0;
+  };
+  if (question10Value === "python") {
+    resultsPython = resultsPython + 1;
+  } else {
+    resultsPython = resultsPython + 0;
+  };
+  if (question11Value === "python") {
+    resultsPython = resultsPython + 1;
+  } else {
+    resultsPython = resultsPython + 0;
+  };
   console.log("python should be: " + resultsPython);
   return resultsPython;
 }
@@ -42,6 +90,14 @@ function calculateRuby() {
   const question1Value = document.querySelector("input[name='name']:checked").value;
   const question2Value = document.querySelector("input[name='name2']:checked").value;
   const question3Value = document.querySelector("input[name='name3']:checked").value;
+  const question4Value = document.querySelector("input[name='name4']:checked").value;
+  const question5Value = document.querySelector("input[name='name5']:checked").value;
+  const question6Value = document.querySelector("input[name='name6']:checked").value;
+  const question7Value = document.querySelector("input[name='name7']:checked").value;
+  const question8Value = document.querySelector("input[name='name8']:checked").value;
+  const question9Value = document.querySelector("input[name='name9']:checked").value;
+  const question10Value = document.querySelector("input[name='name10']:checked").value;
+  const question11Value = document.querySelector("input[name='name11']:checked").value;
   if (question1Value === "ruby") {
     resultsRuby = resultsRuby + 1;
   } else {
@@ -57,6 +113,46 @@ function calculateRuby() {
   } else {
     resultsRuby = resultsRuby + 0;
   };
+  if (question4Value === "ruby") {
+    resultsRuby = resultsRuby + 1;
+  } else {
+    resultsRuby = resultsRuby + 0;
+  };
+  if (question5Value === "ruby") {
+    resultsRuby = resultsRuby + 1;
+  } else {
+    resultsRuby = resultsRuby + 0;
+  };
+  if (question6Value === "ruby") {
+    resultsRuby = resultsRuby + 1;
+  } else {
+    resultsRuby = resultsRuby + 0;
+  };
+  if (question7Value === "ruby") {
+    resultsRuby = resultsRuby + 1;
+  } else {
+    resultsRuby = resultsRuby + 0;
+  };
+  if (question8Value === "ruby") {
+    resultsRuby = resultsRuby + 1;
+  } else {
+    resultsRuby = resultsRuby + 0;
+  };
+  if (question9Value === "ruby") {
+    resultsRuby = resultsRuby + 1;
+  } else {
+    resultsRuby = resultsRuby + 0;
+  };
+  if (question10Value === "ruby") {
+    resultsRuby = resultsRuby + 1;
+  } else {
+    resultsRuby = resultsRuby + 0;
+  };
+  if (question11Value === "ruby") {
+    resultsRuby = resultsRuby + 1;
+  } else {
+    resultsRuby = resultsRuby + 0;
+  };
   console.log("ruby should be: " + resultsRuby);
   return resultsRuby;
 }
@@ -66,6 +162,14 @@ function calculateC() {
   const question1Value = document.querySelector("input[name='name']:checked").value;
   const question2Value = document.querySelector("input[name='name2']:checked").value;
   const question3Value = document.querySelector("input[name='name3']:checked").value;
+  const question4Value = document.querySelector("input[name='name4']:checked").value;
+  const question5Value = document.querySelector("input[name='name5']:checked").value;
+  const question6Value = document.querySelector("input[name='name6']:checked").value;
+  const question7Value = document.querySelector("input[name='name7']:checked").value;
+  const question8Value = document.querySelector("input[name='name8']:checked").value;
+  const question9Value = document.querySelector("input[name='name9']:checked").value;
+  const question10Value = document.querySelector("input[name='name10']:checked").value;
+  const question11Value = document.querySelector("input[name='name11']:checked").value;
   if (question1Value === "c") {
     resultsC = resultsC + 1;
   } else {
@@ -77,6 +181,46 @@ function calculateC() {
     resultsC = resultsC + 0;
   };
   if (question3Value === "c") {
+    resultsC = resultsC + 1;
+  } else {
+    resultsC = resultsC + 0;
+  };
+  if (question4Value === "c") {
+    resultsC = resultsC + 1;
+  } else {
+    resultsC = resultsC + 0;
+  };
+  if (question5Value === "c") {
+    resultsC = resultsC + 1;
+  } else {
+    resultsC = resultsC + 0;
+  };
+  if (question6Value === "c") {
+    resultsC = resultsC + 1;
+  } else {
+    resultsC = resultsC + 0;
+  };
+  if (question7Value === "c") {
+    resultsC = resultsC + 1;
+  } else {
+    resultsC = resultsC + 0;
+  };
+  if (question8Value === "c") {
+    resultsC = resultsC + 1;
+  } else {
+    resultsC = resultsC + 0;
+  };
+  if (question9Value === "c") {
+    resultsC = resultsC + 1;
+  } else {
+    resultsC = resultsC + 0;
+  };
+  if (question10Value === "c") {
+    resultsC = resultsC + 1;
+  } else {
+    resultsC = resultsC + 0;
+  };
+  if (question11Value === "c") {
     resultsC = resultsC + 1;
   } else {
     resultsC = resultsC + 0;
