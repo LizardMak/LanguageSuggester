b6049122a8ccf7b70042184081af8ea209caaf37
Hunter Empey <LizardMak@gmail.com>
2023-10-20 16:42:59 -0600

    refactored code

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,60 @@
-window.addEventListener("load", function() {
-  document.getElementById("radio-form").addEventListener("submit", getResults);
-})
+//Business logic
 
 function calculateResults() {
   let results = 0;
-  if (document.querySelector("input[name='name']:checked").value === "positive") {
+  if (document.querySelector("input[name='question1']:checked").value === "positive") {
     results = results + 1;
-  } else if (document.querySelector("input[name='name']:checked").value === "negative") {
+  } else if (document.querySelector("input[name='question1']:checked").value === "negative") {
     results = results - 1;
   };
-  if (document.querySelector("input[name='name2']:checked").value === "positive") {
+  if (document.querySelector("input[name='question2']:checked").value === "positive") {
     results = results + 1;
-  } else if (document.querySelector("input[name='name2']:checked").value === "negative") {
+  } else if (document.querySelector("input[name='question2']:checked").value === "negative") {
     results = results - 1;
   };
-  if (document.querySelector("input[name='name3']:checked").value === "positive") {
+  if (document.querySelector("input[name='question3']:checked").value === "positive") {
     results = results + 1;
-  } else if (document.querySelector("input[name='name3']:checked").value === "negative") {
+  } else if (document.querySelector("input[name='question3']:checked").value === "negative") {
     results = results - 1;
   };
-  if (document.querySelector("input[name='name4']:checked").value === "positive") {
+  if (document.querySelector("input[name='question4']:checked").value === "positive") {
     results = results + 1;
-  } else if (document.querySelector("input[name='name4']:checked").value === "negative") {
+  } else if (document.querySelector("input[name='question4']:checked").value === "negative") {
     results = results - 1;
   };
-  if (document.querySelector("input[name='name5']:checked").value === "positive") {
+  if (document.querySelector("input[name='question5']:checked").value === "positive") {
     results = results + 1;
     document.getElementById("sus").removeAttribute("class");
-  } else if (document.querySelector("input[name='name5']:checked").value === "negative") {
+  } else if (document.querySelector("input[name='question5']:checked").value === "negative") {
     results = results - 1;
     document.getElementById("sus").setAttribute("class", "hidden");
   };
-  if (document.querySelector("input[name='name6']:checked").value === "positive") {
+  if (document.querySelector("input[name='question6']:checked").value === "positive") {
     results = results + 1;
-  } else if (document.querySelector("input[name='name6']:checked").value === "negative") {
+  } else if (document.querySelector("input[name='question6']:checked").value === "negative") {
     results = results - 1;
   };
-  if (document.querySelector("input[name='name7']:checked").value === "positive") {
+  if (document.querySelector("input[name='question7']:checked").value === "positive") {
     results = results + 1;
-  } else if (document.querySelector("input[name='name7']:checked").value === "negative") {
+  } else if (document.querySelector("input[name='question7']:checked").value === "negative") {
     results = results - 1;
   };
-  if (document.querySelector("input[name='name8']:checked").value === "positive") {
+  if (document.querySelector("input[name='question8']:checked").value === "positive") {
     results = results + 1;
-  } else if (document.querySelector("input[name='name8']:checked").value === "negative") {
+  } else if (document.querySelector("input[name='question8']:checked").value === "negative") {
     results = results - 1;
   };
-  console.log("should be: " + results)
-  return results; 
+  return results;
 }
 //UI logic
+window.addEventListener("load", function () {
+  document.getElementById("radio-form").addEventListener("submit", getResults);
+})
+
+
 function getResults(e) {
   e.preventDefault();
-  window.alert("edefaultworking");
   let finalResults = calculateResults();
-  console.log("is: " + finalResults);
   if (finalResults >= -2 && finalResults <= 2) {
     document.getElementById("result").setAttribute('class', 'resultC');
     document.getElementById("python").setAttribute('class', 'hidden');
@@ -74,4 +74,4 @@ function getResults(e) {
     document.getElementById("ruby").setAttribute('class', 'sizeLimit');
     document.getElementById("result").innerText = "We recommend that you learn Ruby!";
   }
-  }
+}
