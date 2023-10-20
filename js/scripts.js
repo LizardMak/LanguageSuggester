747192be850aac11db7a37ca6e4d37214cebaa36
Hunter Empey <LizardMak@gmail.com>
2023-10-20 10:09:53 -0600

    add Python result calculator

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,18 @@ window.addEventListener("load", function() {
 function getResults(e) {
 e.preventDefault();
 window.alert("edefaultworking");
+let resultsPython = calculatePython();
+console.log(resultsPython);
 }
 
+function calculatePython() {
+  let resultsPython = 0;
+  const question1Value = document.querySelector("input[name='name']:checked").value;
+  if (question1Value === "python") {
+    resultsPython = resultsPython + 1;
+  } else {
+    resultsPython = resultsPython + 0;
+  };
+  console.log(resultsPython);
+  return resultsPython.toString();
+}
